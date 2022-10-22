341910c7cd8817b093eee7a488fe7646960a591d
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-22 14:12:56 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
+require "pry"
 # Your code goes here!
+class Anagram 
+    attr_reader :word
+    def initialize (word)
+        @word = word
+    end
+    # returns an array of words that are anagram of the word passed
+  def match(arr_of_words)
+    return nil if arr_of_words.class != Array
+    return arr_of_words.filter do |word|
+             word.downcase.chars.sort == @word.downcase.chars.sort
+           end
+  end
+end
+listen = Anagram.new ('listen')
+puts listen.match(%w[enlists google inlets banana])
+binding.pry
